b8e070e394f5261bdb18799991c539db8fc3e4dc
zhuqianye <q835041058@gmail.com>
2017-07-22 22:10:15 +0800

    rewrite my readme.md

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import TodoApp from './TodoApp';
 import './index.css';
 ReactDOM.render(<TodoApp />, document.getElementById('root'));

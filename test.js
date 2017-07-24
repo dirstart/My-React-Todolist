782a0d45434d292fa2742cdfb4e0ed2d41135347
zhuqianye <q835041058@gmail.com>
2017-07-24 14:33:59 +0800

    checkout new progress for gh-pages

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-// let t = 8;
+var obj = {
+	a: 1,
+	b: 2
+}
 
-// setTimeout(() => {
-// 	t = 100;
-// 	console.log(t);
-// }, 4000);  //经测试这个t在外围可修改
-
-
-let t;
-clearTimeout(t); //经测试这样不报错，回去再查一下关于clearTimeout的细节
+const obj2 = {
+	...obj,
+	c: 3
+}
+console.log(obj2);
